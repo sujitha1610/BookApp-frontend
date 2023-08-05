0c40a2a336415622dd47b1b703cfcd2f8217a77c
sujitha1610 <sujithamannepalli@gmail.com>
2023-08-05 09:18:55 +0530

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { UploadComponent } from './upload/upload.component';
+import { DisplayComponent } from './display/display.component';
+import { ProfileComponent } from './profile/profile.component';
+import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
+import { DeleteComponent } from './delete/delete.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  
+  {path:"",component:LoginComponent},
+  {path:"home",component:HomeComponent},
+  {path:"upload",component:UploadComponent},
+  {path:"display",component:DisplayComponent},
+  {path:"profile/:id",component:ProfileComponent},
+  {path:"delete",component:DeleteComponent}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,32 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UploadComponent } from './upload/upload.component';
+import { DisplayComponent } from './display/display.component';
+import { HeaderComponent } from './header/header.component';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { ProfileComponent } from './profile/profile.component';
+import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
+import { DeleteComponent } from './delete/delete.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UploadComponent,
+    DisplayComponent,
+    HeaderComponent,
+    ProfileComponent,
+    LoginComponent,
+    HomeComponent,
+    DeleteComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
